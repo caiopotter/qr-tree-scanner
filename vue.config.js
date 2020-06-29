73d4315af413d53191171c53b1a87543405a8828
caiopotter <limapotter@gmail.com>
2020-06-29 16:56:08 -0300

    teste em https localhost

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
+const fs = require('fs')
+
 module.exports = {
+  devServer: {
+    https: {
+      key: fs.readFileSync('./certs/example.com+6-key.pem'),
+      cert: fs.readFileSync('./certs/example.com+6.pem'),
+    },
+    public: 'https://localhost:8080/'
+},
   "transpileDependencies": [
     "vuetify"
   ]
