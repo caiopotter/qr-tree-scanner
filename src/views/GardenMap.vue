66c0e4cd770b11de437ccf593b16217b6b2abb9f
caiopotter <limapotter@gmail.com>
2020-11-20 20:05:12 -0300

    Testing html map

--- src/views/GardenMap.vue
+++ src/views/GardenMap.vue
@@ -1,18 +1,23 @@
 <template>
   <v-container>
       
-        
-          <div class="img-wrapper">
-            <img src="../assets/mapaBosque.jpeg" usemap="#usemap">
+        <v-card max-height="400">
+          <v-card-title><span>Titulo</span></v-card-title>
+          <v-card-actions>
+            <v-row>
+            <v-col class="img-wrapper">
+              <img src="../assets/mapaBosque.jpeg" usemap="#usemap">
 
-            <map name="usemap">
-              <area @click="showMapBottomSheet('Árvore 1')" target="" alt="Arvore 1" title="Arvore 1" href="#" coords="567,838,13" shape="circle">
-              <area @click="showMapBottomSheet('Árvore 2')" target="" alt="Arvore 2" title="Arvore 2" href="#" coords="719,1009,11" shape="circle">
-              <area @click="showMapBottomSheet('Árvore 3')" target="" alt="Arvore 3" title="Arvore 3" href="#" coords="777,750,11" shape="circle">
-              <area @click="showMapBottomSheet('Árvore 4')" target="" alt="Arvore 4" title="Arvore 4" href="#" coords="689,448,10" shape="circle">
-            </map>
-          </div>
-       
+              <map name="usemap">
+                <area @click="showMapBottomSheet('Árvore 1')" target="" alt="Arvore 1" title="Arvore 1" href="#" coords="567,838,13" shape="circle">
+                <area @click="showMapBottomSheet('Árvore 2')" target="" alt="Arvore 2" title="Arvore 2" href="#" coords="719,1009,11" shape="circle">
+                <area @click="showMapBottomSheet('Árvore 3')" target="" alt="Arvore 3" title="Arvore 3" href="#" coords="777,750,11" shape="circle">
+                <area @click="showMapBottomSheet('Árvore 4')" target="" alt="Arvore 4" title="Arvore 4" href="#" coords="689,448,10" shape="circle">
+              </map>
+            </v-col>
+            </v-row>
+          </v-card-actions>
+        </v-card>
       
         <v-bottom-sheet v-model="mapBottomSheet">
           <v-card>
@@ -66,15 +71,15 @@ export default {
 <style>
 .img-wrapper {
     overflow: auto;
-    position: absolute;
+    position: inherit;
     top: 0%;
     left: 0%;
 }
 
 .img-wrapper > img {
-    height: 200%;
-    width: 200%;
-    vertical-align: bottom;
+    height: 150%;
+    width: 150%;
+    
 }
 
 </style>
