667936ec58933adc61b5bc5a2c4b4dd8893a16aa
caiopotter <limapotter@gmail.com>
2020-12-10 00:25:51 -0300

    removing / from api url

--- src/api/ParkApi.js
+++ src/api/ParkApi.js
@@ -1,6 +1,6 @@
 export default{
     getParks(){
-      return axios.get('/api/park/');
+      return axios.get('/api/park');
     },
     getParkTrees(parkId){
       return axios.get(`/api/park/trees/${parkId}`)
